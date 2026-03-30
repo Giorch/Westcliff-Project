dbcb14f797e43118c31ef5e8c7749f1a08213126
Giorch <69166473+Giorch@users.noreply.github.com>
2026-03-29 22:54:45 -0400

    Player & Enemy initial movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currPoint;
+    public float speed;
+    private SpriteRenderer sp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currPoint = pointB.transform;
+        anim.SetBool("isWalking", true);
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currPoint.position - transform.position;
+        if (currPoint == pointB.transform)
+        {
+            rb.linearVelocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currPoint.position) < 0.5f && currPoint == pointB.transform)
+        {
+            currPoint = pointA.transform;
+            sp.flipX = true;
+        }
+        if (Vector2.Distance(transform.position, currPoint.position) < 0.5f && currPoint == pointA.transform)
+        {
+            currPoint = pointB.transform;
+            sp.flipX = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MovementSystem : MonoBehaviour
+{
+    public InputSystem_Actions actions;
+    private float xPosLastFrame;
+    public float speed;
+    public float jumpForce;
+    public float maxSpeed;
+    float move;
+    Rigidbody2D rb;
+    public Transform groundCheckTransform;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    bool isGrounded;
+    SpriteRenderer spriteRenderer;
+    [SerializeField] private Animator _animator;
+    private void Awake()
+    {
+        xPosLastFrame = transform.position.x;
+        actions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        actions.Player.Enable();
+        actions.Player.Move.performed += Movement;
+        actions.Player.Jump.performed += Jumping;
+        actions.Player.Sprint.performed += Running;
+
+        actions.Player.Move.canceled += Movement;
+        actions.Player.Jump.canceled += Jumping;
+    }
+    private void OnDisable()
+    {
+        actions.Player.Disable();
+        actions.Player.Move.performed -= Movement;
+        actions.Player.Jump.performed -= Jumping;
+    }
+
+    void Running (InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && isGrounded)
+        {
+            
+        }
+        else if (!isGrounded)
+        {
+            
+        }
+    }
+    void Movement(InputAction.CallbackContext ctx)
+    {
+        move = ctx.ReadValue<Vector2>().x;
+        if (move != 0)
+        {
+            _animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
+        }
+    }
+
+    void Jumping(InputAction.CallbackContext ctx)
+    {
+        if(ctx.performed && isGrounded)
+        {
+            rb.linearVelocityY = jumpForce;
+        }
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundLayer);
+        if (transform.position.x > xPosLastFrame)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (transform.position.x < xPosLastFrame)
+        {
+            spriteRenderer.flipX = true;
+        }
+        xPosLastFrame = transform.position.x;
+    }
+    private void FixedUpdate()
+    {
+        Run();
+    }
+    private void Run()
+    {
+        var velocity = move * speed;
+        rb.linearVelocityX = velocity;
+        
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheckTransform.position, groundCheckRadius);
+    }
+}
